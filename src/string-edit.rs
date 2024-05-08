e91ba91da92a4bbd688d13cff76e77585bb26cd9
kekelp <kekelp@kekelp>
2024-05-08 22:26:09 +0200

    7

---
+++ src/string-edit.rs
@@ -0,0 +1,76 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+pub fn backspace(string: &mut String, cursor: usize) -> usize {
+    let previous_grapheme = string[0..cursor].grapheme_indices(true).rev().next();
+    if let Some((prev_idx, _prev_grapheme)) = previous_grapheme {
+        string.replace_range(prev_idx..cursor, "");
+        return prev_idx;
+    }
+    return cursor;
+}
+
+pub fn ctrl_backspace_unicode_word(string: &mut String, cursor: usize) -> usize {
+    let previous_grapheme = string[0..cursor].unicode_word_indices().rev().next();
+    if let Some((prev_idx, _prev_grapheme)) = previous_grapheme {
+        string.replace_range(prev_idx..cursor, "");
+        return prev_idx;
+    }
+    return cursor;
+}
+
+pub fn left_arrow(string: &mut String, cursor: usize) -> usize {
+    let previous_grapheme = string[0..cursor].grapheme_indices(true).rev().next();
+    if let Some((prev_idx, _prev_grapheme)) = previous_grapheme {
+        return prev_idx;
+    }
+    return cursor;
+}
+
+pub fn right_arrow(string: &mut String, cursor: usize) -> usize {
+    let previous_grapheme = string[0..cursor].grapheme_indices(true).next();
+    if let Some((prev_idx, _prev_grapheme)) = previous_grapheme {
+        return prev_idx;
+    }
+    return cursor;
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_backspace() {
+        let results = ["Sneed", "need", "Seed", "Sned", "Sned", "Snee"];
+
+        for i in 0..5 {
+            let mut string = String::from("Sneed");
+            let new_cursor = backspace(&mut string, i);
+            assert_eq!(string, results[i]);
+            let expected_new_cursor = i.saturating_sub(1);
+            assert_eq!(new_cursor, expected_new_cursor);
+        }
+
+        let results = [
+            "種子と飼料",
+            "子と飼料",
+            "種と飼料",
+            "種子飼料",
+            "種子と料",
+            "種子と飼",
+        ];
+        let source = "種子と飼料";
+        let indices: Vec<usize> = source.char_indices().map(|x| x.0).collect();
+
+        for (i, idx) in indices.iter().enumerate() {
+            let mut string = String::from(source);
+            let new_cursor = backspace(&mut string, *idx);
+            assert_eq!(string, results[i]);
+            let expected_new_cursor = indices[i.saturating_sub(1)];
+            assert_eq!(new_cursor, expected_new_cursor);
+        }
+    }
+}
