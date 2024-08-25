060bfadc57828f6737f890dba333889455cf742d
kekelp <kekelp@kekelp>
2024-08-25 17:02:07 +0200

    15

--- src/main_ui.rs
+++ src/main_ui.rs
@@ -1,40 +1,37 @@
 // crate::* is needed to fix some crap with macros: https://github.com/rust-lang/rust/pull/52234#issuecomment-894851497
 // when ui will be in its own crate, this won't happen anymore
 use crate::node_params::*;
+use crate::ui::Len::*;
 use crate::ui::Position::*;
 use crate::ui::Size::*;
-use crate::ui::Len::*;
 use crate::ui::*;
 use crate::*;
 use glyphon::{cosmic_text::Align, Attrs, Color as GlyphonColor, Family, Weight};
 use view_derive::node_key;
 
-const COLOR1: Color =  Color::rgba(50, 13, 100, 240);
-const COLOR2: Color =  Color::rgba(100, 13, 50, 240);
+const COLOR1: Color = Color::rgba(50, 13, 100, 240);
+const COLOR2: Color = Color::rgba(100, 13, 50, 240);
 const GRAD1: VertexColors = VertexColors::diagonal_gradient_forward_slash(COLOR1, COLOR2);
 const FLGR_PANEL: NodeParams = PANEL.vertex_colors(GRAD1);
 
-
 impl State {
-    pub fn update_ui(&mut self) {       
-
+    pub fn update_ui(&mut self) {
         tree!(self.ui, {
-
             #[node_key(PANEL)]
             const SLIDER: NodeKey;
-            
+
             if let Some((x, _y)) = self.ui.is_dragged(SLIDER) {
                 self.slider_value -= x as f32;
             }
 
             add!(self.ui, SLIDER, {
                 text!(self.ui, "Slider");
-            }).set_position_x(Position::Static(Pixels(self.slider_value as u32)));
+            })
+            .set_position_x(Position::Static(Pixels(self.slider_value as u32)));
 
             #[node_key(V_STACK.position_x(Position::End).size_y(Fill).size_x(FitContent).stack_arrange(Arrange::Center))]
             const SIDEBAR: TypedKey<Stack>;
-            add!(self.ui, SIDEBAR, {            
-
+            add!(self.ui, SIDEBAR, {
                 self.add_tools();
 
                 self.add_pixel_info_ui();
@@ -81,10 +78,8 @@ impl CounterState {
     #[node_key(LABEL)]
     pub const COUNT_LABEL_2: NodeKey;
 
-
     // pub const COUNT_LABEL: TypedKey<Text> =
     // TypedKey::<Text>::new(&LABEL.debug_name("COUNT_LABEL"), Id(8379459943087886814)).validate();
-
 
     // pub const COUNT_LABEL: TypedKey<Text> = TypedKey::new(&LABEL.debug_name("COUNT_LABEL"), Id(4286411996384850605)).validate();
 
@@ -96,13 +91,11 @@ pub fn count_color(count: i32) -> Color {
     return Color::rgba(red, 26, 52, 205);
 }
 
-
 impl State {
     pub fn add_twin_thing_ui(&mut self) {
         #[node_key(FLGR_PANEL.size_y(Fixed(Frac(0.5))).position_x(Position::Start))]
         const PIXEL_PANEL: NodeKey;
         add!(self.ui, PIXEL_PANEL, {
-
             v_stack!(self.ui, {
                 for _ in 0..3 {
                     text!(self.ui, "Identical");
@@ -113,7 +106,6 @@ impl State {
 }
 
 impl State {
-
     pub fn add_counter_ui(&mut self) {
         margin!(self.ui, {
             #[node_key(H_STACK.position_x(Position::Center))]
@@ -125,11 +117,15 @@ impl State {
                         add!(self.ui, CounterState::INCREASE_BUTTON).set_color(new_color);
 
                         let count = &self.counter_state.count.to_string();
-                        add!(self.ui, CounterState::COUNT_LABEL).set_text(count).set_text_attrs(
-                            Attrs::new().family(Family::SansSerif).color(GlyphonColor::rgb(255, 76, 23)).weight(Weight::EXTRA_BOLD)
-
-                        )
-                        .set_text_align(Align::Center);
+                        add!(self.ui, CounterState::COUNT_LABEL)
+                            .set_text(count)
+                            .set_text_attrs(
+                                Attrs::new()
+                                    .family(Family::SansSerif)
+                                    .color(GlyphonColor::rgb(255, 76, 23))
+                                    .weight(Weight::EXTRA_BOLD),
+                            )
+                            .set_text_align(Align::Center);
 
                         add!(self.ui, CounterState::DECREASE_BUTTON);
                     }
@@ -162,7 +158,10 @@ impl State {
         let pixel_info = &self.canvas.pixel_info();
 
         let (x, y) = match pixel_info {
-            Some(pixel_info) => (format!("x: {}", pixel_info.x), format!("y: {}", pixel_info.y)),
+            Some(pixel_info) => (
+                format!("x: {}", pixel_info.x),
+                format!("y: {}", pixel_info.y),
+            ),
             None => ("x:  ".to_owned(), "y:  ".to_owned()),
         };
 
@@ -180,12 +179,10 @@ impl State {
         #[node_key(FLGR_PANEL.position_x(End).position_y(Start).size_x(FitContentOrMinimum(Pixels(100))))]
         const PIXEL_PANEL2: NodeKey;
         add!(self.ui, PIXEL_PANEL2, {
-            
             v_stack!(self.ui, {
                 text!(self.ui, &x);
                 text!(self.ui, &y);
             });
-
         });
     }
 
@@ -196,21 +193,16 @@ impl State {
         #[node_key(ICON_BUTTON.image(include_bytes!("icons/eraser.png")))]
         const ERASER_ICON: NodeKey;
 
-        
         #[node_key(FLGR_PANEL.position_x(End).position_y(Start).size_x(FitContent))]
         const TOOLS_PANEL: NodeKey;
 
         add!(self.ui, TOOLS_PANEL, {
-
             h_stack!(self.ui, {
-
                 v_stack!(self.ui, {
                     add!(self.ui, BRUSH_ICON);
                     add!(self.ui, ERASER_ICON);
                 });
-
             });
-
         });
 
         if self.ui.is_clicked(BRUSH_ICON) {
@@ -223,11 +215,9 @@ impl State {
     }
 
     pub fn add_stacks_test(&mut self) {
-
         #[node_key(H_STACK.stack_arrange(Arrange::Start))]
         const HSTACK1: NodeKey;
-        add!(self.ui, HSTACK1, {                
-        
+        add!(self.ui, HSTACK1, {
             #[node_key(LABEL)]
             const LABEL7: TypedKey<Text>;
 
@@ -259,7 +249,6 @@ impl State {
             #[node_key(V_STACK.stack_arrange(Arrange::Start))]
             const VSTACK5567: NodeKey;
             add!(self.ui, VSTACK5567, {
-
                 #[node_key(H_STACK.stack_arrange(Arrange::Start))]
                 const HSTACK_A: NodeKey;
                 add!(self.ui, HSTACK_A, {
@@ -283,7 +272,6 @@ impl State {
                     add!(self.ui, LABEL7).set_text("z2");
                     add!(self.ui, LABEL7).set_text("z3");
                 });
-
             });
         });
     }
@@ -316,5 +304,4 @@ impl State {
             });
         });
     }
-
 }
