76020c0cc64550d5d70fd6a7859a82a28237263d
kekelp <kekelp@kekelp>
2024-11-11 21:30:04 +0100

    0

--- examples/counter.rs
+++ examples/counter.rs
@@ -1,4 +1,4 @@
-use blue::{example_window_loop::*, V_STACK};
+use blue::example_window_loop::*;
 use blue::{Color, NodeKey, Ui, BUTTON, LABEL};
 use blue::node_key;
 use winit::error::EventLoopError;
@@ -11,12 +11,10 @@ pub struct State {
 
 impl ExampleLoop for State {
     fn declare_ui(&mut self, ui: &mut Ui) {
-        // Some light calculations to turn the real state (self.count, self.show) into things that the Ui understands (String, Color)
         fn count_color(count: i32) -> Color {
             let red = (0.1 * (count as f32) * 255.0) as u8;
             return Color::rgba(red, 26, 52, 205);
         }
-        let color = count_color(self.count);
 
         let show_button_text = match self.show {
             true => "Hide Counter",
@@ -27,41 +25,34 @@ impl ExampleLoop for State {
         // We're mostly just using the default params in BUTTON here.
         // Using a NodeKey to assign a stable identity to each element is almost always a good idea, but it's not always necessary.
         #[node_key] const INCREASE: NodeKey;
-        // #[node_key] const DECREASE: NodeKey;
+        #[node_key] const DECREASE: NodeKey;
         #[node_key] const LABELKEY: NodeKey;
         #[node_key] const SHOW: NodeKey;
-
         #[node_key] const VSTACKKEY: NodeKey;
 
         // ui.add(DECREASE).params(BUTTON).static_text("Decrease");
         ui.add(INCREASE)
             .params(BUTTON)
-            .color(color)
-            .on_click(|| { self.count += 1 })
+            .color(count_color(self.count))
             .static_text("Increase");
-        ui.add(LABELKEY)
-            .params(LABEL)
-            .color(Color::RED)
-            .text(self.count);
+
         ui.add(SHOW)
             .params(BUTTON)
             .color(Color::RED)
             .static_text(show_button_text);
 
-        ui.add(VSTACKKEY).params(V_STACK);
+        ui.add(DECREASE)
+            .params(BUTTON)
+            .static_text("Decrease");
 
-
-        // ui.place(SHOW);
-
-        // Declare the layout.
 
         ui.v_stack().nest(|| {
             if self.show {
                 ui.place(INCREASE);
-                ui.place(LABELKEY); 
-
-                // this one is all in one line cuz we're quirky like that
-                ui.add_anon(BUTTON).static_text("Decrease").on_click(|| { self.count -= 1 }).place();
+                // this one sets its appearance and place()s itself into the tree in the same line, just for fun.
+                // since we don't need to check its clicks either, this means that we can do it without a key.
+                ui.add_anon(LABEL).text(self.count).place();
+                ui.place(DECREASE);
 
             }
             ui.place(SHOW);
@@ -73,12 +64,12 @@ impl ExampleLoop for State {
         if ui.is_clicked(SHOW) {
             self.show = !self.show;
         }
-        // if ui.is_clicked(INCREASE) {
-        //     self.count += 1;
-        // }
-        // if ui.is_clicked(DECREASE) {
-        //     self.count -= 1;
-        // }
+        if ui.is_clicked(INCREASE) {
+            self.count += 1;
+        }
+        if ui.is_clicked(DECREASE) {
+            self.count -= 1;
+        }
     }
 }
 
