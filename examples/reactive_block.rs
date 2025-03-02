4ab45f6a3c30f084df2c688944aac7a62ab530bd
kekelp <kekelp@kekelp>
2025-03-02 15:52:16 +0100

    1

--- examples/reactive_block.rs
+++ examples/reactive_block.rs
@@ -3,43 +3,29 @@ use keru::example_window_loop::*;
 
 #[derive(Default)]
 pub struct State {
-    pub count_1: Observer<i32>,
-    pub show_1: Observer<bool>,
-
-    pub count_2: Observer<i32>,
-    pub show_2: Observer<bool>,
+    pub count: Observer<i32>,
 }
 
 pub trait CustomComponents {
-    fn counter(&mut self, count: &mut Observer<i32>, show: &mut Observer<bool>);
+    fn counter(&mut self, count: &mut Observer<i32>);
 }
 
 impl CustomComponents for Ui {
-    fn counter(&mut self, count: &mut Observer<i32>, show: &mut Observer<bool>) {
-        
+    fn counter(&mut self, count: &mut Observer<i32>) {
+        #[node_key] const INCREASE: NodeKey;
+        #[node_key] const DECREASE: NodeKey;
+        #[node_key] const SHOW: NodeKey;
+
         self.subtree().start(|| {
 
-            let changed = self.check_changes(count) || self.check_changes(show);
+            let changed = self.check_changes(count);
             reactive(changed, || {
 
-                #[node_key] const INCREASE: NodeKey;
-                #[node_key] const DECREASE: NodeKey;
-                #[node_key] const SHOW: NodeKey;
-
-                if self.is_clicked(SHOW) {
-                    *show = ! *show;
+                if is_in_skipped_reactive_block() {
+                    log::warn!("Reactive block: dependencies unchanged");
+                } else {
+                    log::warn!("Reactive block: dependencies changed");
                 }
-                if self.is_clicked(INCREASE) {
-                    *count += 1;
-                }
-                if self.is_clicked(DECREASE) {
-                    *count -= 1;
-                }
-
-                let show_button_text = match **show {
-                    true => "Hide Counter",
-                    false => "Show Counter",
-                };
 
                 let red = 0.1 * (**count as f32);
                 let count_color = Color::rgba_f(red, 0.10196, 0.59608, 0.80392);
@@ -47,28 +33,17 @@ impl CustomComponents for Ui {
                     .color(count_color)
                     .static_text("Increase")
                     .key(INCREASE);
-    
-                let show_button = BUTTON
-                    .color(Color::RED)
-                    .static_text(show_button_text)
-                    .key(SHOW);
-        
-                let decrease_button = BUTTON
-                    .static_text("Decrease")
-                    .key(DECREASE);
+
+                self.add(increase_button);
+                
+                if self.is_clicked(INCREASE) {
+                    *count += 1;
+                }
 
 
-                let label = LABEL.text(count);
+            });
 
-                self.v_stack().nest(|| {
-                    if **show {
-                        self.add(increase_button);
-                        self.add(label);
-                        self.add(decrease_button);
-                    }
-                    self.add(show_button);
-                });
-            });    
+
         });
     }
 }
@@ -77,10 +52,7 @@ impl CustomComponents for Ui {
 impl ExampleLoop for State {
     fn update_ui(&mut self, ui: &mut Ui) {
         
-        ui.h_stack().nest(|| {
-            ui.counter(&mut self.count_1, &mut self.show_1);
-            ui.counter(&mut self.count_2, &mut self.show_2);
-        });
+        ui.counter(&mut self.count);
 
     }
 }
@@ -88,7 +60,7 @@ impl ExampleLoop for State {
 fn main() {
     env_logger::Builder::new()
         .filter_level(log::LevelFilter::Warn)
-        .filter_module("keru::node_params", log::LevelFilter::Trace)
+        .filter_module("keru::reactive", log::LevelFilter::Trace)
         .init();
     
     let state = State::default();
--- src/reactive.rs
+++ src/reactive.rs
@@ -66,12 +66,8 @@ impl Ui {
         let cosmetic_hash = self.nodes[i].params.cosmetic_hash();
         let layout_hash = self.nodes[i].params.layout_hash();
         
-
-
         let cosmetic_changed = cosmetic_hash != self.nodes[i].last_cosmetic_hash;
         let layout_changed = layout_hash != self.nodes[i].last_layout_hash;
-
-
 
         #[cfg(debug_assertions)]
         if reactive::is_in_skipped_reactive_block() {
@@ -82,12 +78,13 @@ impl Ui {
                     (false, true) => "appearance",
                     _ => unreachable!()
                 };
-                dbg!(self.nodes[i].params.rect.vertex_colors, params.params.rect.vertex_colors);
-                dbg!(self.nodes[i].params.cosmetic_hash(), params.params.cosmetic_hash());
-                dbg!(self.nodes[i].last_cosmetic_hash);
-                dbg!(self.nodes[i].params.rect.vertex_colors == params.params.rect.vertex_colors);
-                dbg!(cosmetic_changed);
-                log::error!("Keru: incorrect reactive block: the {kind} params of node \"{}\" changed, even if a reactive block declared that it shouldn't have.\n Check that the reactive block is correctly checking all the runtime variables that can affect the node's params.", self.node_debug_name(i));
+                // dbg!(self.nodes[i].params.rect.vertex_colors, params.params.rect.vertex_colors);
+                // dbg!(self.nodes[i].params.cosmetic_hash(), params.params.cosmetic_hash());
+                // dbg!(self.nodes[i].last_cosmetic_hash);
+                // dbg!(self.nodes[i].params.rect.vertex_colors == params.params.rect.vertex_colors);
+                // dbg!(cosmetic_changed);
+                log::error!("Keru: incorrect reactive block: the {kind} params of node \"{}\" changed, but reactive thought they didn't", self.node_debug_name(i));
+                // log::error!("Keru: incorrect reactive block: the {kind} params of node \"{}\" changed, even if a reactive block declared that it shouldn't have.\n Check that the reactive block is correctly checking all the runtime variables that can affect the node's params.", self.node_debug_name(i));
             }
             return;
         }
