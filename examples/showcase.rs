ebdfe5cb2b562cdbddd78889bb678e7c31f6a311
kekelp <kekelp@kekelp>
2025-04-03 21:12:18 +0200

    nicer intro

--- examples/showcase.rs
+++ examples/showcase.rs
@@ -32,13 +32,13 @@ trait Components {
 impl Components for Ui {
     fn intro_tab(&mut self, state: &mut State) {
         self.add(V_SCROLL_STACK).nest(|| {
-            self.static_paragraph(JAPANESE_TEXT);
+            self.static_paragraph("Keru is an experimental GUI library focused on combining a simple and natural programming model with high performance and flexibility.");
             
             #[node_key] const TEXT_EDIT_1: NodeKey;
-            #[node_key] const TEXT_EDIT_2: NodeKey;
+            // #[node_key] const TEXT_EDIT_2: NodeKey;
 
             self.add(TEXT_EDIT.key(TEXT_EDIT_1));
-            self.add(TEXT_EDIT.key(TEXT_EDIT_2));
+            // self.add(TEXT_EDIT.key(TEXT_EDIT_2));
 
             self.static_paragraph("Here are some basic GUI elements: \n");
             self.static_paragraph("Button and label:");
