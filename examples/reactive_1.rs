b8b7f74d2ecd4e2ab2f3a2bac52321e6df16f100
kekelp <kekelp@kekelp>
2026-04-15 14:27:51 +0200

    more words

--- examples/reactive_1.rs
+++ examples/reactive_1.rs
@@ -1,7 +1,11 @@
-/// This is an example showing how to use `readd_branch` for "reactivity".
+/// This is an example showing how to use the `Ui::readd_branch()` function for "reactivity".
 /// Note that this feature is experimental and it doesn't fit in the library all that well. In particular, you can't use it together with Components.
+/// It also requires you to do some manual change tracking, although there is an `Observer` struct that can help.
+/// Generally, I don't think that true reactivity can implemented in a good and transparent way without conflicting with Keru's goal of a simple library that doesn't impose any restrictions on how the program's state is arranged, doesn't use any compiler plugins or other "magic", and lets the user always control if and when his code is executed. (Well, the last point is literally the opposite of what reactivity is). 
+/// 
+/// The point of this example is mostly to try to convince you that you don't need it.
 ///
-/// Normally, a GUI program has a state that can evolve in arbitrary ways. But once the state is set, it's usually not hard to go from the state to the GUI representation. In Keru, you just have to create a few `Node` values, maybe format some strings (which is fast if you just use an arena), and call a few `ui.add` functions, which are very cheap. Then the library can take it from there and do diffing, incremental relayouts, or incremental updates to the render data, if it wants to.
+/// Normally, a GUI program has a state that can evolve in arbitrary ways. But once the state is set, it's usually not hard to go from the state to the GUI representation. In Keru, you just have to create a few `Node`s on the stack, maybe format some strings (which is fast if you just use an arena), and call a few `ui.add()` functions, which are very cheap. Then the library can take it from there and do diffing, incremental relayouts, or incremental updates to the render data, if it wants to.
 /// 
 /// As long as this is the case, "reactivity" isn't really anything that we need to worry about, and it's not worth complicating the programming model for it.
 ///
@@ -10,20 +14,20 @@
 ///
 /// The easy solution would be to just store the converted form in our state, and update when it changes. That would probably work great, so maybe we still don't need reactivity. But what if we were like, really convinced that this reactivity thing was the future? In that case, we might want a way to skip running that part of the GUI rebuilding code completely, except on the frames where the underlying `i32` changed.
 ///
-/// To be precise, if it was so expensive that it makes the program actually miss frames, that would be a separate issue: we'd still miss the frame whenever the `i32` did change. So we'd need a real solution, like computing it in a separate thread. (See the async_thread.rs example.)
+/// To be precise, if it was so expensive that it makes the program actually miss frames, that would be a separate issue: we'd still miss the frame whenever the `i32` did change. So we'd need a real solution like computing it in a separate thread and showing a spinner or something while it's not ready. (See the async_thread.rs example.)
 /// 
-/// Also, if computing that expensive value was the ONLY thing that our program does, it wouldn't be a problem either. If the window loop is set correctly, it doesn't rerun the GUI rebuild code at all unless the `Ui` received an input that it cares about. If it only cares about the Increase button being clicked, that means that it's already only running it when the value actually needs to be recomputed, and there'd be nothing to skip.
+/// Also, if computing that expensive value was the ONLY thing that our program does, it wouldn't be a problem either. If the window loop is set correctly, it doesn't rerun the GUI rebuild code at all unless the `Ui` received an input that it cares about. If it only cares about the Increase button being clicked, that means that it's already running it only when the value actually needs to be recomputed, and there'd be nothing to skip.
 /// 
-/// What we're talking about here is the very specific case where we have a complex GUI with many individual parts, it's common for the user to interact with simpler parts of the GUI in ways that don't change the state that the expensive part depends on, and the cost of rerunning the expensive parts when not needed is adding up in terms of CPU usage or power consumption.
+/// What we're talking about here is the very specific case where we have a complex GUI with many individual parts, and it's common for the user to interact with simpler parts of the GUI in ways that don't change the whole state, and the cost of rerunning the expensive parts when not needed is adding up in terms of CPU usage or power consumption.
 /// In this case it might finally make sense to think about rerunning the builder code for the simple part but skipping the code for the expensive part.
 /// 
 /// (But remember that it's never too late to just cache the result of the expensive calculation, stop thinking about reactivity, and move on to more interesting things.)
 /// 
 /// If we really decided that we want to do this, we can do it using just one function: `ui.readd_branch()`, which turns off the usual "clear the tree and redeclare it" mechanism and instead keeps the whole branch the way it was in the previous frame.
 ///
-/// To know when to rebuild and when to skip, we need to do some basic change-tracking ourselves. In this example, we use an `Observer`, a simple wrapper struct that does it automatically using DerefMut. But we could also do it manually with a basic boolean flag.
+/// To know when to rebuild and when to skip, we need to do some basic change-tracking ourselves. In this example, we use an `Observer`, a simple wrapper struct that does it automatically using `DerefMut`. But we could also do it manually with a basic boolean flag.
 ///
-/// Note that if we put the code that increases `reactive_count` inside the `if changed {}` block, it would be skipped as well! because of this, we lose some of the usual flexibility and we are forced to cleanly separate the GUI rebuild code from the effects.
+/// Note that if we put the code that increases `reactive_count` inside the `if changed {}` block, it would be skipped as well! That would break our program completely. So we lose some of the usual flexibility and we are forced to separate the GUI rebuild code from the effects.
 ///
 /// This also means that we can't use Components and `readd_branch()` at the same time. To write a Component, you have to mix their GUI code and their effects into a single `add_to_ui` function, which makes it impossible to split it. To fix this, we'd have to change the Component trait and force everyone to split their code into two separate functions. Right now, I don't think it's worth it to complicate Component for this.
 
