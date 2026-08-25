011af077dd85b5192b6230c720995ae4729f42af
kekelp <kekelp>
2026-08-25 18:21:45 +0200

    Simplify component example

--- examples/03_components.rs
+++ examples/03_components.rs
@@ -1,20 +1,20 @@
 use keru::*;
 use keru::node_library::*;
 
-/// This example shows how to use the 'Component` trait.
-/// 
-/// Components are the most robust way of separating GUI code into reusable "components".
-/// In addition, Components can have their own "local" state.
-/// This is an advanced feature: most programs will be fine without it.
-///
-/// In a complex program, it could surely get annoying if the user had to add a field in their State
-/// for every state variable of every color picker, rich text editor,
-/// or every small self-contained widget that they have.
+// This example shows how to use the 'Component` trait.
+// 
+// Components are the most robust way of separating GUI code into reusable "components" or "widgets".
+// In addition, Components can hold their own "local" state.
+// This is an advanced feature: most programs will be fine without it.
+//
+// In a complex program, it could surely get annoying if the user had to add a field in their State
+// for every state variable of every color picker, rich text editor,
+// or every small self-contained widget that they have.
 
 
-/// Using a Component is meant to feel like building a regular Node and adding it.
-/// A component is a Node-like struct that describes the component's parameters
-/// We can make it hold references to portions of the outside state.
+// Using a Component is meant to feel like building a regular Node and adding it.
+// A component is a Node-like struct that describes the component's parameters
+// We can make it hold references to portions of the outside state.
 pub struct StatefulCounter {
     pub color: Color,
 }
@@ -81,30 +81,30 @@ fn main() {
 }
 
 
-/// Components are still experimental, and there's lots of different ways to use them.
-/// 
-/// - We can use the AddResult type so that ui.add_component() returns a value.
-///   The returned value can be an output, such as the selected color of a color picker. 
-///   It can also be an `UiParent`, so that the caller can add nodes as children of the component:
-///       
-///       ui.add_component(container_component).nest(|| { ... })`.
-/// 
-/// - We can stick references to the outside state inside the component's descriptor struct, 
-///   so that the component impl can mutate it. 
-///   For example, a Slider component can hold a `&mut f32` in its struct.
-/// 
-/// - We can also add a `ComponentKey` to the component struct,
-///   implement the optional `component_key()` method to let the trait know where to find it,
-///   and then implement the `Component::run_component()` method and return the output from there.
-///   Then, the user can call `ui.run_component(COMPONENT_KEY)` to run that logic and get the output.
-///   
-///   This system is currently a little clunky, but it's the way that will feel most similar 
-///   to adding a node with a key calling `ui.is_clicked(key)` on it:
-/// 
-///     #[component_key] const COMPONENT_KEY;
-///     let counter = Counter::default().with_key(COMPONENT_KEY);
-///
-///     ui.add_component(counter);
-///
-///     if let Some(output) = ui.run_component(COUNTER_1) { ... }
-///
+// Components are still experimental, and there's lots of different ways to use them.
+// 
+// - We can use the AddResult type so that ui.add_component() returns a value.
+//   The returned value can be an output, such as the selected color of a color picker. 
+//   It can also be an `UiParent`, so that the caller can add nodes as children of the component:
+//       
+//       ui.add_component(container_component).nest(|| { ... })`.
+// 
+// - We can stick references to the outside state inside the component's descriptor struct, 
+//   so that the component impl can mutate it. 
+//   For example, a Slider component can hold a `&mut f32` in its struct.
+// 
+// - We can also add a `ComponentKey` to the component struct,
+//   implement the optional `component_key()` method to let the trait know where to find it,
+//   and then implement the `Component::run_component()` method and return the output from there.
+//   Then, the user can call `ui.run_component(COMPONENT_KEY)` to run that logic and get the output.
+//   
+//   This system is currently a little clunky, but it's the way that will feel most similar 
+//   to adding a node with a key calling `ui.is_clicked(key)` on it:
+// 
+//     #[component_key] const COMPONENT_KEY;
+//     let counter = Counter::default().with_key(COMPONENT_KEY);
+//
+//     ui.add_component(counter);
+//
+//     if let Some(output) = ui.run_component(COUNTER_1) { ... }
+//
